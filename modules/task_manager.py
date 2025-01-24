c0f2caef423e8e8a7eb0a69394b9c84a0e4d033c
ZhenyaYoratt <zhenya.sugonaevr.u@gmail.com>
2025-01-25 00:47:23 +0500

    Process Management refactoring: Added the Process class for processing information about processes, improved error handling, and added methods for terminating, suspending, and resuming processes.

--- modules/task_manager.py
+++ modules/task_manager.py
@@ -1,101 +1,188 @@
 import psutil
+import ctypes
 from .logger import *
+from PyQt5.QtGui import QImage, QPixmap
 
+class Process():
+    STATUS = {
+        psutil.STATUS_RUNNING: "Выполняется",
+        psutil.STATUS_SLEEPING: "Спит",
+        psutil.STATUS_DISK_SLEEP: "Спит (диск)",
+        psutil.STATUS_STOPPED: "Остановлен",
+        psutil.STATUS_TRACING_STOP: "Остановлен (трассировка)",
+        psutil.STATUS_ZOMBIE: "Зомби",
+        psutil.STATUS_DEAD: "Мертв",
+        psutil.STATUS_WAKING: "Пробуждается",
+        psutil.STATUS_IDLE: "Простаивает",
+        psutil.STATUS_LOCKED: "Заблокирован",
+        psutil.STATUS_WAITING: "Ожидает",
+        psutil.STATUS_LOCKED: "Блокирован",
+        psutil.STATUS_PARKED: "Припаркован",
+    }
+    PROCESS_TYPE = {
+        'system': 'Системный',
+        'critical': 'Критический',
+        'normal': 'Обычный',
+    }
+
+    def __init__(self, pid, name, status, cpu_percent, memory_percent, create_time):
+        self.process_type = get_process_type(pid)
+        self.pid = pid
+        self.name = name
+        self.status = status
+        self.cpu_percent = cpu_percent
+        self.memory_percent = memory_percent
+        self.create_time = create_time
+
+    def get_process_icon(self):
+        try:
+            if self.pid == 0:
+                return None
+            # Получение пути к исполняемому файлу процесса
+            process = psutil.Process(self.pid)
+            exe_path = process.exe()
+
+            # Извлечение иконки из исполняемого файла
+            hicon = ctypes.windll.shell32.ExtractIconW(0, exe_path, 0)
+            if hicon:
+                hdc = ctypes.windll.user32.GetDC(0)
+                hdc_mem = ctypes.windll.gdi32.CreateCompatibleDC(hdc)
+                hbm = ctypes.windll.gdi32.CreateCompatibleBitmap(hdc, 32, 32)
+                hbm_old = ctypes.windll.gdi32.SelectObject(hdc_mem, hbm)
+                ctypes.windll.user32.DrawIconEx(hdc_mem, 0, 0, hicon, 32, 32, 0, 0, 3)
+                ctypes.windll.gdi32.SelectObject(hdc_mem, hbm_old)
+                ctypes.windll.gdi32.DeleteDC(hdc_mem)
+                ctypes.windll.user32.ReleaseDC(0, hdc)
+
+                # Преобразование Bitmap в QPixmap
+                class BITMAP(ctypes.Structure):
+                    _fields_ = [
+                        ("bmType", ctypes.c_long),
+                        ("bmWidth", ctypes.c_long),
+                        ("bmHeight", ctypes.c_long),
+                        ("bmWidthBytes", ctypes.c_long),
+                        ("bmPlanes", ctypes.c_ushort),
+                        ("bmBitsPixel", ctypes.c_ushort),
+                        ("bmBits", ctypes.c_void_p)
+                    ]
+
+                bmpinfo = BITMAP()
+                ctypes.windll.gdi32.GetObjectW(hbm, ctypes.sizeof(BITMAP), ctypes.byref(bmpinfo))
+                bmpstr = ctypes.create_string_buffer(bmpinfo.bmWidthBytes * bmpinfo.bmHeight)
+                ctypes.windll.gdi32.GetBitmapBits(hbm, len(bmpstr), bmpstr)
+                image = QImage(bmpstr, bmpinfo.bmWidth, bmpinfo.bmHeight, QImage.Format_ARGB32)
+                pixmap = QPixmap.fromImage(image)
+                return pixmap
+            return None
+        except Exception as e:
+            log(f"Ошибка при получении иконки процесса с PID {self.pid}: {e}", ERROR)
+            return None
+
+    def kill(self):
+        """Завершает процесс по PID."""
+        try:
+            process = psutil.Process(self.pid)
+            process.terminate()
+            msg = f"Процесс {self.name} ({self.pid}) завершен."
+            log(msg)
+            return msg
+        except Exception as e:
+            msg = f"Ошибка завершения процесса {self.name}: {e}"
+            log(msg, ERROR)
+            return msg
+
+    def suspend(self):
+        """
+        Приостанавливает процесс с указанным PID.
+
+        :param pid: ID процесса, который нужно приостановить.
+        :return: Строка с результатом выполнения операции.
+        """
+        try:
+            process = psutil.Process(self.pid)
+            process.suspend()
+            msg = f"Процесс с PID {self.pid} успешно приостановлен."
+            log(msg)
+            return msg
+        except psutil.NoSuchProcess:
+            msg = f"Ошибка: Процесс с PID {self.pid} не существует."
+            log(msg, ERROR)
+            return msg
+        except psutil.AccessDenied:
+            msg = f"Ошибка: Доступ к процессу с PID {self.pid} запрещён."
+            log(msg, ERROR)
+            return msg
+        except Exception as e:
+            msg = f"Ошибка при приостановке процесса с PID {self.pid}: {e}"
+            log(msg, ERROR)
+            return msg
+
+    def resume(self):
+        """
+        Возобновляет приостановленный процесс с указанным PID.
+
+        :param pid: ID процесса, который нужно возобновить.
+        :return: Строка с результатом выполнения операции.
+        """
+        try:
+            process = psutil.Process(self.pid)
+            process.resume()
+            msg = f"Процесс с PID {self.pid} успешно возобновлён."
+            log(msg)
+            return msg
+        except psutil.NoSuchProcess:
+            msg = f"Ошибка: Процесс с PID {self.pid} не существует."
+            log(msg, ERROR)
+            return msg
+        except psutil.AccessDenied:
+            msg = f"Ошибка: Доступ к процессу с PID {self.pid} запрещён."
+            log(msg, ERROR)
+            return msg
+        except Exception as e:
+            msg = f"Ошибка при возобновлении процесса с PID {self.pid}: {e}"
+            log(msg, ERROR)
+            return msg
+
+
+    def __repr__(self):
+        return f"Process({self.pid}, {self.name})"
+    
+    def __str__(self):
+        return f"{self.name} ({self.pid})"
+    
 def get_process_list():
     """Возвращает список активных процессов."""
     processes = []
-    for proc in psutil.process_iter(attrs=["pid", "name", "status"]):
-        processes.append(f"PID: {proc.info['pid']}, Name: {proc.info['name']}, Status: {proc.info['status']}")
+    for proc in psutil.process_iter(attrs=["pid", "name", "status", "cpu_percent", "memory_percent", "create_time"]):
+        processes.append(Process(
+            proc.info['pid'],
+            proc.info['name'],
+            proc.info['status'],
+            proc.info['cpu_percent'],
+            proc.info['memory_percent'],
+            proc.info['create_time']
+        ))
     return processes
 
-def parse_process_info(text):
-    # Разделяем текст на пары ключ-значение
-    parts = text.split(", ")
+class ProcessType:
+    SYSTEM = 'system'
+    CRITICAL = 'critical'
+    NORMAL = 'normal'
 
-    # Инициализируем переменные
-    pid = None
-    name = None
-    status = None
-
-    # Обрабатываем каждую пару
-    for part in parts:
-        key, value = part.split(": ", 1)
-        key = key.strip().lower()  # Приводим ключ к нижнему регистру
-        value = value.strip()
-
-        if key == "pid":
-            pid = int(value)  # Преобразуем PID в число
-        elif key == "name":
-            name = value
-        elif key == "status":
-            status = value
-
-    return pid, name, status
-
-def kill_process(pid):
-    """Завершает процесс по PID."""
+def get_process_type(pid):
     try:
-        p = psutil.Process(pid)
-        p.terminate()
-        msg = f"Процесс {pid} ({p.name()}) завершен."
-        log(msg)
-        return msg
-    except Exception as e:
-        msg = f"Ошибка завершения процесса {pid}: {e}"
-        log(msg, ERROR)
-        return msg
-    
-import psutil
-
-def suspend_process(pid):
-    """
-    Приостанавливает процесс с указанным PID.
-
-    :param pid: ID процесса, который нужно приостановить.
-    :return: Строка с результатом выполнения операции.
-    """
-    try:
+        if pid == 0:
+            return ProcessType.SYSTEM
         process = psutil.Process(pid)
-        process.suspend()
-        msg = f"Процесс с PID {pid} успешно приостановлен."
-        log(msg)
-        return msg
+        if process.username() in ['SYSTEM', 'NT AUTHORITY\\SYSTEM', 'NT AUTHORITY\\СИСТЕМА', 'root', 'СИСТЕМА', 'NT AUTHORITY\\LOCAL SERVICE']:
+            return ProcessType.SYSTEM # Не стесняйтесь добавлять свои значения!
+        elif False: # TODO: Добавить проверку на критические процессы
+            return ProcessType.CRITICAL
+        else:
+            return ProcessType.NORMAL
     except psutil.NoSuchProcess:
-        msg = f"Ошибка: Процесс с PID {pid} не существует."
-        log(msg, ERROR)
-        return msg
-    except psutil.AccessDenied:
-        msg = f"Ошибка: Доступ к процессу с PID {pid} запрещён."
-        log(msg, ERROR)
-        return msg
+        log(f"Процесс с PID {pid} не найден.", WARNING)
+        return None
     except Exception as e:
-        msg = f"Ошибка при приостановке процесса с PID {pid}: {e}"
-        log(msg, ERROR)
-        return msg
-
-def resume_process(pid):
-    """
-    Возобновляет приостановленный процесс с указанным PID.
-
-    :param pid: ID процесса, который нужно возобновить.
-    :return: Строка с результатом выполнения операции.
-    """
-    try:
-        process = psutil.Process(pid)
-        process.resume()
-        msg = f"Процесс с PID {pid} успешно возобновлён."
-        log(msg)
-        return msg
-    except psutil.NoSuchProcess:
-        msg = f"Ошибка: Процесс с PID {pid} не существует."
-        log(msg, ERROR)
-        return msg
-    except psutil.AccessDenied:
-        msg = f"Ошибка: Доступ к процессу с PID {pid} запрещён."
-        log(msg, ERROR)
-        return msg
-    except Exception as e:
-        msg = f"Ошибка при возобновлении процесса с PID {pid}: {e}"
-        log(msg, ERROR)
-        return msg
-
+        log(f"Ошибка при определении типа процесса с PID {pid}: {e}", ERROR)
+        return None
--- ui/task_manager.py
+++ ui/task_manager.py
@@ -1,71 +1,169 @@
 from PyQt5.QtWidgets import *
-from PyQt5.QtCore import Qt
-from modules.task_manager import get_process_list, kill_process, parse_process_info
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from modules.task_manager import get_process_list, get_process_type, Process
 from modules.titles import make_title
+from psutil import boot_time
+
+def parse_precents(value):
+    return f"{value:.1f}%" if value is not None else "0.0%"
+
+def parse_create_time(value):
+    return QDateTime.fromSecsSinceEpoch(int(value)).toString(Qt.DateFormat.ISODate)
+
+class ProcessListWorker(QObject):
+    process_list_updated = pyqtSignal(list)
+    update_interval = 1
+
+    def run(self):
+        while True:
+            self.refresh_process_list()
+            QThread.msleep(int(self.update_interval * 1000))
+
+    def refresh_process_list(self):
+        self.process_list_updated.emit(get_process_list())
 
 class TaskManagerWindow(QMainWindow):
     def __init__(self, parent = None):
         super().__init__()
         self.setParent(parent)
         self.setWindowTitle(make_title("Диспетчер задач"))
-        self.setFixedSize(800, 600)
+        self.resize(1000, 700)
         self.setWindowFlags(Qt.WindowType.Dialog)
         self.setWindowFlag(Qt.WindowType.WindowContextHelpButtonHint, False)
+        self.setStyleSheet(self.styleSheet() + "QPushButton { padding: 0px 4px; }")
 
         layout = QVBoxLayout()
 
+        top_layout = QHBoxLayout()
+        top_layout.addWidget(QLabel("Диспетчер задач"))
+
+        view_button = QPushButton("Вид")
+        view_button.setMenu(QMenu(self))
+        view_button.menu().addAction("Обновить сейчас", self.refresh_process_list)
+        update_interval_menu = view_button.menu().addMenu("Скорость обновления")
+        update_interval_menu.addAction("Очень быстро — 0.1 сек", lambda: self.set_update_interval(0.5))
+        update_interval_menu.addAction("Быстро — 0.5 секунд", lambda: self.set_update_interval(0.5))
+        update_interval_menu.addAction("Обычная — 1 секунд", lambda: self.set_update_interval(1))
+        update_interval_menu.addAction("Низкая — 3 секунд", lambda: self.set_update_interval(5))
+        update_interval_menu.addAction("Очень низкая — 5 секунд", lambda: self.set_update_interval(5))
+        update_interval_menu.addAction("Черепаха — 10 секунд", lambda: self.set_update_interval(10))
+        view_button.menu().addSeparator()
+        hide_critical_processes_action = view_button.menu().addAction("Скрыть критические процессы", self.toggle_critical_processes)
+        hide_critical_processes_action.setCheckable(True)
+        hide_system_processes_action = view_button.menu().addAction("Скрыть системные процессы", self.toggle_system_processes)
+        hide_system_processes_action.setCheckable(True)
+        hide_system_processes_action.setChecked(True)
+
+        # Поиск
+        self.search_bar = QLineEdit()
+        self.search_bar.setPlaceholderText('Поиск названия, файла, PID...')
+        self.search_bar.setClearButtonEnabled(True)
+        self.search_bar.textChanged.connect(lambda text: self.filter_process_list(text))
+
+        boot_time_label = QLabel(f"Время загрузки: {QDateTime.fromSecsSinceEpoch(int(boot_time())).toString(Qt.DateFormat.ISODate)}")
+
+        top_layout.addWidget(view_button)
+        top_layout.addWidget(self.search_bar)
+        top_layout.addWidget(boot_time_label)
+
         # Таблица процессов
         self.process_table = QTableWidget()
-        self.process_table.setColumnCount(4)
-        self.process_table.setHorizontalHeaderLabels(["Имя процесса", "PID", "Статус", "Действия"])
+        self.process_table.setColumnCount(8)
+        self.process_table.setHorizontalHeaderLabels(["Имя процесса", "ЦП", "ОЗУ", "Состояние", "PID", "Тип", "Действия с процессом", "Создано"])
         self.process_table.setSelectionBehavior(self.process_table.SelectRows)
+        self.process_table.setEditTriggers(self.process_table.NoEditTriggers)
+
+        layout.addLayout(top_layout)
         layout.addWidget(self.process_table)
-
-        # Кнопки управления
-        refresh_button = QPushButton("Обновить список")
-        refresh_button.clicked.connect(self.refresh_process_list)
-        kill_button = QPushButton("Завершить процесс")
-        kill_button.clicked.connect(self.terminate_selected_process)
-
-        layout.addWidget(refresh_button)
-        layout.addWidget(kill_button)
 
         central_widget = QWidget()
         central_widget.setLayout(layout)
         self.setCentralWidget(central_widget)
 
-        self.refresh_process_list()
+        # Таймер обновления списка процессов
+        self._worker = ProcessListWorker()
+        self._thread = QThread()
+        self._worker.moveToThread(self._thread)
+        self._worker.process_list_updated.connect(lambda process_list: self.update_process_list(process_list))
+        self._thread.started.connect(self._worker.run)
+        self._thread.start()
+        
+        self.process_list_ui = []
+        self.process_list = []
+
+        self.hide_critical_processes = False
+        self.hide_system_processes = True
+
+        self.setStatusBar(QStatusBar())
+        self.statusBar().showMessage("Диспетчер задач запущен.")
+
+    def closeEvent(self, a0):
+        self._thread.quit()
+        return super().closeEvent(a0)
+    
+    def set_update_interval(self, interval):
+        self._worker.update_interval = interval
+
+    def toggle_critical_processes(self):
+        self.hide_critical_processes = not self.hide_critical_processes
+        self.filter_process_list()
+
+    def toggle_system_processes(self):
+        self.hide_system_processes = not self.hide_system_processes
+        self.filter_process_list()
+
+    def filter_process_list(self, search_text = None):
+        """Фильтрует список процессов по имени процесса, имя файла или PID."""
+        if search_text:
+            self.process_list_ui = [process for process in self.process_list if search_text.lower() in process.name.lower() or search_text in str(process.pid)]
+        if self.hide_critical_processes:
+            self.process_list_ui = [process for process in self.process_list_ui if process.process_type != 'critical']
+        if self.hide_system_processes:
+            self.process_list_ui = [process for process in self.process_list_ui if process.process_type != 'system']
+        return self.refresh_process_list()
+    
+    def update_process_list(self, process_list):
+        self.process_list = process_list
+        if self.search_bar.text():
+            self.filter_process_list(self.search_bar.text())
+        else:
+            self.process_list_ui = self.process_list
+        self.filter_process_list()
 
     def refresh_process_list(self):
         """Обновляет список процессов."""
-        process_list = get_process_list()
-        self.process_table.setRowCount(len(process_list))
-        for row, process in enumerate(process_list):
-            pid, name, status = parse_process_info(process)
-            self.process_table.setItem(row, 0, QTableWidgetItem(name))
-            self.process_table.setItem(row, 1, QTableWidgetItem(str(pid)))
-            self.process_table.setItem(row, 2, QTableWidgetItem(status))
+        count = len(self.process_list)
+        count_ui = len(self.process_list_ui)
+        self.statusBar().showMessage("Всего: " + str(count) + ". Показано процессов: " + str(count_ui) + " (из них скрыты: " + str(count - count_ui) + ")")
+        self.process_table.setRowCount(count_ui)
+        for row, process in enumerate(self.process_list_ui):
+            process: Process = process
+            item = QTableWidgetItem(process.name)
+            item.setIcon(QIcon(process.get_process_icon()))
+            self.process_table.setItem(row, 0, item)
+            self.process_table.setItem(row, 1, QTableWidgetItem(parse_precents(process.cpu_percent) if process.pid != 0 else None))
+            self.process_table.setItem(row, 2, QTableWidgetItem(parse_precents(process.memory_percent)))
+            self.process_table.setItem(row, 3, QTableWidgetItem(Process.STATUS[process.status]))
+            self.process_table.setItem(row, 4, QTableWidgetItem(str(process.pid)))
+            self.process_table.setItem(row, 5, QTableWidgetItem(Process.PROCESS_TYPE[process.process_type]))
+            self.process_table.setItem(row, 7, QTableWidgetItem(parse_create_time(process.create_time)))
             actions = QWidget()
             layout_actions = QHBoxLayout()
             terminate_button = QPushButton('Завершить')
-            terminate_button.clicked.connect(lambda: kill_process(pid))
+            terminate_button.clicked.connect(process.kill)
             suspend_button = QPushButton('Приостановить')
+            suspend_button.clicked.connect(process.suspend)
             resume_button = QPushButton('Возобновить')
+            resume_button.clicked.connect(process.resume)
             layout_actions.addWidget(terminate_button)
             layout_actions.addWidget(suspend_button)
             layout_actions.addWidget(resume_button)
+            layout_actions.setContentsMargins(0, 0, 0, 0)
             actions.setLayout(layout_actions)
             actions.setContentsMargins(0, 0, 0, 0)
-            self.process_table.setItem(row, 3, QTableWidgetItem())
-            self.process_table.setCellWidget(row, 3, actions)
+            self.process_table.setItem(row, 6, QTableWidgetItem())
+            self.process_table.setCellWidget(row, 6, actions)
         self.process_table.resizeColumnsToContents()
         self.process_table.resizeRowsToContents()
 
-    def terminate_selected_process(self):
-        """Завершает выбранный процесс."""
-        selected_row = self.process_table.currentRow()
-        if selected_row < 0:
-            return
-        pid = self.process_table.item(selected_row, 1).text()
-        result = kill_process(int(pid))
-        self.refresh_process_list()
